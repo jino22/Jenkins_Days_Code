565a704edfd0a277ef4586bb3fe5ce906662e281
jino22 <jinoyajiv.com>
2024-01-13 00:20:44 +0530

    Python days code added

---
+++ Python_Days.py
@@ -0,0 +1,6 @@
+# Displaying days of the week from Monday to Sunday
+
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+for day in days_of_week:
+    print(day)
